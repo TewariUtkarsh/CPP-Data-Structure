277011a89d6f96e5bbcbfc134cf89a6605e79c01
UT <tewariutkarsh@outlook.com>
2021-03-07 13:49:41 +0530

    Linked List in C++

---
+++ SingleLinked.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+typedef struct node NODE;
+
+void traverse(NODE *ptr)
+{
+    cout<<"\nElements: \n";
+    while(ptr!=NULL)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+NODE* insertbeg(NODE* head,int data)
+{
+    NODE *ptr=(NODE*)malloc(sizeof(NODE *));
+    ptr->next=head;
+    ptr->data=data;
+    return ptr;
+}
+NODE* insertbet(NODE* head,int data,int i)
+{
+    NODE*ptr;
+    ptr=(NODE*)malloc(sizeof(NODE));
+    int j=0;
+    while(j<i-1)
+    {
+        head=head->next;
+        ++j;
+    }
+    ptr->next=head->next;
+    head->next=ptr;
+    ptr->data=data;
+    return ptr; 
+}
+NODE* insertend(NODE* end,int n)
+{
+    NODE* ptr;
+    ptr=(NODE*)malloc(sizeof(NODE));
+    end->next=ptr;
+    ptr->next=NULL;
+    ptr->data=n;
+    return ptr;
+}
+NODE* delbeg(NODE*head)
+{
+    NODE *ptr;
+    ptr=(NODE*)malloc(sizeof(NODE));
+    ptr->next=head;
+    head=head->next;
+    free(ptr);
+    return head;
+}
+NODE* delbet(NODE* head,int n)
+{
+    NODE* ptr;
+    ptr=(NODE*)malloc(sizeof(NODE));
+    int i=0;
+    while(i<n-2)
+    {
+        head=head->next;
+        ++i;
+    }
+    ptr=head->next;
+    head->next=ptr->next;
+    free(ptr);
+    return head;
+}
+NODE* delend(NODE* head)
+{
+    NODE*p,*q;
+    p=(NODE*)malloc(sizeof(NODE));
+    q=(NODE*)malloc(sizeof(NODE));
+    q=head;
+    p=head->next;
+    while(p->next!=NULL)
+    {
+        p=p->next;
+        q=q->next;
+    }
+    q->next=NULL;
+    free(p);
+    return q;
+}
+int main()
+{
+    NODE *head,*temp,*first;
+    first=NULL;
+    head=NULL;
+    cout<<"Enter the elements:\n";
+    for(int i=0;i<5;++i)
+    {
+        temp=(NODE*)malloc(sizeof(NODE));
+        cin>>temp->data;
+        if(head!=NULL)
+        {
+            head->next=temp;
+            head=temp;
+            temp->next=NULL;
+        }
+        else
+        {
+            head=first=temp;
+        }
+    }
+    head=first;
+    head=insertbeg(head,0);
+    first=head;
+    temp=insertend(temp,6);
+    head=insertbet(head,55,4);
+    head=first;
+    head=delbeg(head);
+    first=head;
+    temp=delend(head);
+    head=delbet(head,4);
+    head=first;
+    traverse(head);
+    return 0;
+}
